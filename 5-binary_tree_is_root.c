080de548c42bf288235f33102a1a4bdcc7a9e910
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-11-30 19:23:10 -0500

    ADD: is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - check if a node is a root 
+ * @node: node for check
+ * Return: 1 if is a root, otherwise 0.
+ */
+int binary_tree_is_root(binary_tree_t *node)
+{
+	if (node != NULL && node->parent == NULL)
+		return(1);
+	return(0);
+}
