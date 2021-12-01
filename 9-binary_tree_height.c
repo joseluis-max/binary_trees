02766e09ba3c784788832dfdc4bb16b97653b242
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-01 16:17:10 -0500

    ADD: height files

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,11 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left = 0;
+	size_t right = 0;
+
 	if (tree == NULL || (!tree->left && !tree->right))
 		return (0);
-	size_t left = binary_tree_height(tree->left);
-	size_t right = binary_tree_height(tree->right);
-
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 	if (left > right)
 		return (left + 1);
 	else
