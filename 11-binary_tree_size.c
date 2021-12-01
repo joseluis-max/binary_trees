0118890cb2e58059f534537a1411bdf953ca354f
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-01 16:24:39 -0500

    ADD: size files

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,11 +8,11 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t left = 0;
-	size_t rigth = 0;
+	size_t right = 0;
 
 	if (tree == NULL)
 		return (0);
-	letf = binary_tree_size(tree->left);
+	left = binary_tree_size(tree->left);
 	right = binary_tree_size(tree->right);
 
 	return (left + right + 1);
