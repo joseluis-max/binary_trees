ce6ab174d88feb270b120b723d77c6e8f9688504
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-01 22:56:25 -0500

    ADD: is perfect files

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -49,6 +49,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	size_t depth = 0;
 	int level = 0;
 
+	if (tree == NULL)
+		return (0);
 	depth = binary_tree_height(tree);
 	return (helper_recursion(tree, depth, level));
 }
