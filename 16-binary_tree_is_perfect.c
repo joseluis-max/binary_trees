be08cfb1b5da6a232d8c99a3d62c90e67a3b9e93
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-01 22:55:06 -0500

    ADD: is perfect files

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_height - calculate binary tree's height
+ * @tree: node binary tree's root
+ * Return: height binary tree (size_t)
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left = 0;
