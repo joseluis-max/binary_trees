2ac6c9c145b40cc0498a13c6fae79da0067e70a3
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-01 22:53:31 -0500

    ADD: is perfect files

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,19 @@
 #include "binary_trees.h"
 
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left = 0;
+	size_t right = 0;
+
+	if (tree == NULL || (!tree->left && !tree->right))
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left > right)
+		return (left + 1);
+	else
+		return (right + 1);
+}
 /**
  * helper_recursion - check if every leaf have the same depth
  * @tree: node binary tree's root
