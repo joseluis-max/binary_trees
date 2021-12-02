1b141b668d3271f623017a31a6990c93e246f40f
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-01 22:58:16 -0500

    ADD: siblings files

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - find a sibling
+ * @node: node is a pointer to the node to find the sibling
+ * Return: pointer to sinbling node or NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == NULL || node->parent->right == NULL)
+		return  (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
