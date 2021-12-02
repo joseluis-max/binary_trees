d8eb26e2fb9e053f9e1e8aa3357dd61e5949eaf0
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2021-12-01 22:08:34 -0500

    ADD: leaves files

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,13 +7,16 @@
  */
 size_t binary_tree_leaves(binary_tree_t *tree)
 {
+	size_t left = 0;
+	size_t right = 0;
+	
 	if (tree == NULL)
 		return (0);
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	size_t left = binary_tree_leaves(tree->left);
-	size_t right = binary_tree_leaves(tree->right);
+	left = binary_tree_leaves(tree->left);
+	right = binary_tree_leaves(tree->right);
 
 	return (left + right);
 }
